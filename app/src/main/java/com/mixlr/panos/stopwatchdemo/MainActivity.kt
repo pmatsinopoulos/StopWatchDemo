5176c702099f0b9a019b2d94f43fd1721862e7c1
Panos Matsinopoulos <panos@mixlr.com>
2023-06-06 11:55:55 +0300

    Added start/stop/reset basic functions

--- app/src/main/java/com/mixlr/panos/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/stopwatchdemo/MainActivity.kt
@@ -6,10 +6,47 @@ import com.mixlr.panos.stopwatchdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isStarted: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            btnStart.setOnClickListener {
+                startOrStop()
+            }
+
+            btnReset.setOnClickListener {
+                reset()
+            }
+        }
+    }
+
+    private fun startOrStop() {
+        if (isStarted) {
+            stop()
+        } else {
+            start()
+        }
+    }
+    private fun start() {
+        binding.apply {
+            btnStart.text = "Stop"
+            isStarted = true
+        }
+    }
+
+    private fun stop() {
+        binding.apply {
+            btnStart.text = "Start"
+            isStarted = false
+        }
+    }
+
+    private fun reset() {
+        binding.apply {
+            stop()
+        }
     }
 }
